eb6525910b070562f94116253b284ab5df817b49
Justin Wong <jusw85@hotmail.com>
2022-04-02 19:50:08 +0800

    Update

--- spawner.gd
+++ spawner.gd
@@ -14,8 +14,8 @@ func _ready():
 
 func _spawn():
 	var pos := Vector2()
-	pos.x = rand_range(0, _extents.x) - (_extents.x/2) + position.x
-	pos.y = rand_range(0, _extents.y) - (_extents.y/2) + position.y
+	pos.x = rand_range(0, _extents.x) - (_extents.x/2)
+	pos.y = rand_range(0, _extents.y) - (_extents.y/2)
 
 	var spawn = _enemies[randi() % 2].instance()
 	spawn.position = pos
