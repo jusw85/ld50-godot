dfbc0a570c9bf933b2c95cdaa438ad28535fa302
Justin Wong <jusw85@hotmail.com>
2022-04-03 16:57:50 +0800

    Update

---
+++ background/background.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+const row = preload("res://background/row.tscn")
+var rows := []
+var player
+
+func _ready():
+	var y = 0;
+	while y < get_viewport().size.y:
+		var obj = row.instance()
+		obj.position.y = y
+		add_child(obj)
+		rows.push_back(obj)
+		y += 64
+
+func _process(_delta):
+	if player == null:
+		var a = get_tree().get_nodes_in_group("player")
+		if a.size() > 0:
+			player = a.front()
+
+	for row in rows:
+		if player != null:
+			row.position.y += player.speed * _delta
+
+	if rows.back().position.y > get_viewport().size.y:
+		var row = rows.pop_back()
+		row.position.y = rows.front().position.y - 64
+		rows.push_front(row)
